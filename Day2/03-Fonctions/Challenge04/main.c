4dcd71270a899d45df97872a112621eb138ce57e
Taha Irhmourra <irhmourrataha@outlook.com>
2025-09-07 00:14:34 +0100

    Dans le quatrième challenge, j’ai appris à créer une fonction pour retourner le minimum de deux nombres.

---
+++ Day2/03-Fonctions/Challenge04/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int valMin(int a,int b) {
+    if (a < b) return a;
+    else return b;
+}
+
+int main () {
+    int a, b;
+    printf("Veuillez enter la valeur A: ");
+    scanf("%d", &a);
+    printf("Veuillez enter la valeur B: ");
+    scanf("%d", &b);
+    printf("La valeur minimum est: %d", valMin(a, b));
+    return 0;
+}
