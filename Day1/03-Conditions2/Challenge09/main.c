4e4963275317b1dcb6f018bd01b6c1f95c021ce3
Taha Irhmourra <irhmourrataha@outlook.com>
2025-09-06 19:54:34 +0100

    Dans le neuvième challenge, j’ai appris à calculer le salaire complet avec heures supplémentaires et primes selon le poste.Dans le neuvième défi, j’ai appris à calculer le salaire complet avec heures supplémentaires et primes selon le poste.

--- Day1/03-Conditions2/Challenge09/main.c
+++ Day1/03-Conditions2/Challenge09/main.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 
 int main () {
-    float tempC;
-    printf("Veuillez enter la temperature en Celisius: ");
-    scanf("%f", &tempC);
-    if (tempC < 0) {
-        printf("L'etat de l'eau est solid");
-    } else if (0 <= tempC && tempC < 100) {
-        printf("L'etat de l'eau est liquide");
-    } else {
-        printf("L'etat de l'eau est gaz");
+    float smig, bonus, salaire;
+    int heures_sup, choix;
+    smig = 3046.77;
+
+    printf("Veuillez enter le nombre d'heures supplementaire: ");
+    scanf("%d", &heures_sup);
+    bonus = (17.10 * 1.5) * heures_sup;
+
+    do {
+        printf("Veuillez indice votre poste\n1-Senior\n2-Junior\n=> ");
+        scanf("%d", &choix);
+    } while (choix != 1 && choix != 2);
+
+    switch(choix) {
+        case 1: salaire = smig + bonus + ((smig * 20) / 100);
+        break;
+        case 2: salaire = smig + bonus + ((smig * 10) / 100);
+        break;
     }
-    return 0;
+
+    printf("Votre Salaire est %.2f DH", salaire);
 }
