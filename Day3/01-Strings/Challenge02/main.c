d7a52f9692fdea36da8f43282ed6391f4dff8f62
Taha Irhmourra <irhmourrataha@outlook.com>
2025-09-09 12:04:32 +0100

    Dans le deuxième challenge, j’ai appris à calculer la longueur d’une chaîne sans utiliser de fonction prédéfinie.

--- Day3/01-Strings/Challenge02/main.c
+++ Day3/01-Strings/Challenge02/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main () {
     char chaineChars[100], i=0, counter=0;
     printf("Veuillez enter une chaine de caracteres: ");
     fgets(chaineChars, sizeof(chaineChars), stdin);
-    do {
-        counter++;-
-        i++;
-    } while(chaineChars[i] != 0);
-    printf("La longueur de la chaine est: %d", counter - 1);
+    int taille = strlen(chaineChars);
+    if (chaineChars[taille - 1] = '\n') chaineChars[taille - 1] = '\0';
+    taille = strlen(chaineChars);
+    printf("La longueur de la chaine est: %d", taille);
     return 0;
 }
