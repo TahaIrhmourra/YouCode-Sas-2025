fe65e1db5ffc795a5464457521efaec7a3f5c082
Taha Irhmourra <irhmourrataha@outlook.com>
2025-09-12 23:03:34 +0100

    Terminer projet final

--- Projet-Fin-SAS/main.c
+++ Projet-Fin-SAS/main.c
@@ -18,22 +18,58 @@ struct Joueurs {
     int numeroPost;
     int ageJoueur;
     int numeroButs;
+    int jourInc;
+    char moisInc[25];
+    int anneeInc;
+    char status[25];
 };
 
-struct Joueurs joueur[50];
-int numeroJoueurs = 0;
-int id = 1;
+struct Joueurs joueur[50] = {
+    {1, "Messi", "Lionel", 10, "Attaquant", 1, 36, 700, 10, "Juin", 2004, "titulaire"},
+    {2, "Ronaldo", "Cristiano", 7, "Attaquant", 1, 39, 820, 12, "Juillet", 2002, "titulaire"},
+    {3, "Mbappe", "Kylian", 11, "Attaquant", 1, 26, 250, 20, "Aout", 2015, "remplacant"},
+    {4, "Haaland", "Erling", 9, "Attaquant", 1, 24, 180, 5, "Janvier", 2019, "titulaire"},
+    {5, "Neymar", "Junior", 10, "Gardien", 2, 32, 400, 13, "Mai", 2010, "titulaire"},
+    {6, "Modric", "Luka", 19, "Milieu", 3, 39, 150, 2, "Mars", 2003, "remplacant"},
+    {7, "Salah", "Mohamed", 11, "Attaquant", 2, 33, 230, 8, "Avril", 2012, "titulaire"},
+    {8, "Kane", "Harry", 9, "Attaquant", 1, 31, 310, 14, "Fevrier", 2011, "titulaire"},
+    {9, "DeBruyne", "Kevin", 17, "Gardien", 3, 34, 160, 7, "Septembre", 2009,"remplacant"},
+    {10,"Hakimi", "Achraf", 2, "Defenseur", 4, 26, 60, 3, "Octobre", 2017, "titulaire"}
+};
+
+int numeroJoueurs = 10;
+int id = 11;
 
 // Ajouter
 void AjouterUnJoueur() {
-    int choix1=0, choix2;
+    int choix1=0, choix2=0, choixDate=0;
+    // id
     joueur[numeroJoueurs].idUnique = id;
+    // nom
     printf("Veuillez enter le nom de joueur %d: ", numeroJoueurs + 1);
     scanf("%s", joueur[numeroJoueurs].nom);
+    // prenom
     printf("Veuillez enter le prenom de joueur %d: ", numeroJoueurs + 1);
     scanf("%s", joueur[numeroJoueurs].prenom);
-    printf("Veuillez enter le numero Maillot de joueur %d: ", numeroJoueurs + 1);
-    scanf("%d", &joueur[numeroJoueurs].numeroMaillot);
+    // maillot
+    int doubleMaillot = 1;
+    do {
+        int i, Nmaillot;
+            printf("Veuillez enter le numero Maillot de joueur %d: ", numeroJoueurs + 1);
+            scanf("%d", &Nmaillot);
+            for (i = 0; i < numeroJoueurs; i++) {
+                if (joueur[i].numeroMaillot == Nmaillot) {
+                    doubleMaillot = 1;
+                    printf("\nNumero do maillot deja utiliser\n\n");
+                    break;
+                } else {
+                    joueur[numeroJoueurs].numeroMaillot = Nmaillot;
+                    doubleMaillot = 0;
+                break;
+                }
+            }
+    } while (doubleMaillot == 1);
+    // poste
     printf("1 - gardien\n");
     printf("2 - defenseur\n");
     printf("3 - milieu\n");
@@ -54,12 +90,40 @@ void AjouterUnJoueur() {
                 joueur[numeroJoueurs].numeroPost = 4;
         break;
     }
+    // age
     printf("Veuillez enter l'age de joueur %d: ", numeroJoueurs + 1);
     scanf("%d", &joueur[numeroJoueurs].ageJoueur);
+    // butd
     printf("Veuillez enter le numero de buts de joueur %d: ", numeroJoueurs + 1);
     scanf("%d", &joueur[numeroJoueurs].numeroButs);
+    // date d'inscription
+    printf("Veuillez enter la date d'inscription de joueur %d", numeroJoueurs + 1);
+        // jour
+    printf("\n1 - enter le jour: ");
+    scanf("%d", &joueur[numeroJoueurs].jourInc);
+        // mois
+    char lesMois[12][15] = {"Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"};
+    printf("2 - enter le mois: ");
+    int moisEnter;
+    scanf("%d", &moisEnter);
+    strcpy(joueur[numeroJoueurs].moisInc, lesMois[moisEnter - 1]);
+        // annee
+    printf("1 - enter l'annee: ");
+    scanf("%d", &joueur[numeroJoueurs].anneeInc);
+    // statue
+    printf("1 - titulaire\n");
+    printf("2 - remplaçant\n");
+    printf("Veuillez indique si le joueur %d est"GREEN" titulaire "RESET"ou" RED" remplacant"RESET": ", numeroJoueurs + 1);
+    scanf("%d", &choix2);
+    switch (choix2) {
+        case 1: strcpy(joueur[numeroJoueurs].status, "titulaire");
+        break;
+        case 2: strcpy(joueur[numeroJoueurs].status, "remplacant");
+        break;
+    }
     id++;
     numeroJoueurs++;
+    printf("\n\n");
 }
 void AjouterPlusieursJoueur() {
     int choix1=0, choix2;
@@ -67,13 +131,33 @@ void AjouterPlusieursJoueur() {
     printf("Veuillez enter le nombre des joueurs qui tu veux ajouter: ");
     scanf("%d", &nombreAjouter);
     do {
+        // id
         joueur[numeroJoueurs].idUnique = id;
+        // nom
         printf("Veuillez enter le nom de joueur %d: ", numeroJoueurs + 1);
         scanf("%s", joueur[numeroJoueurs].nom);
+        // prenom
         printf("Veuillez enter le prenom de joueur %d: ", numeroJoueurs + 1);
         scanf("%s", joueur[numeroJoueurs].prenom);
-        printf("Veuillez enter le numero Maillot de joueur %d: ", numeroJoueurs + 1);
-        scanf("%d", &joueur[numeroJoueurs].numeroMaillot);
+        // maillot
+        int doubleMaillot = 1;
+        do {
+            int i, Nmaillot;
+            printf("Veuillez enter le numero Maillot de joueur %d: ", numeroJoueurs + 1);
+            scanf("%d", &Nmaillot);
+            for (i = 0; i < numeroJoueurs; i++) {
+                if (joueur[i].numeroMaillot == Nmaillot) {
+                    doubleMaillot = 1;
+                    printf("\nNumero do maillot deja utiliser\n\n");
+                    break;
+                } else {
+                    joueur[numeroJoueurs].numeroMaillot = Nmaillot;
+                    doubleMaillot = 0;
+                break;
+                }
+            }
+        } while (doubleMaillot == 1);
+        // poste
         printf("1 - gardien\n");
         printf("2 - defenseur\n");
         printf("3 - milieu\n");
@@ -94,10 +178,37 @@ void AjouterPlusieursJoueur() {
                     joueur[numeroJoueurs].numeroPost = 4;
             break;
         }
+        // age
         printf("Veuillez enter l'age de joueur %d: ", numeroJoueurs + 1);
         scanf("%d", &joueur[numeroJoueurs].ageJoueur);
+        // butd
         printf("Veuillez enter le numero de buts de joueur %d: ", numeroJoueurs + 1);
         scanf("%d", &joueur[numeroJoueurs].numeroButs);
+        // date d'inscription
+        printf("Veuillez enter la date d'inscription de joueur %d", numeroJoueurs + 1);
+            // jour
+        printf("\n1 - enter le jour: ");
+        scanf("%d", &joueur[numeroJoueurs].jourInc);
+            // mois
+        char lesMois[12][15] = {"Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"};
+        printf("2 - enter le mois: ");
+        int moisEnter;
+        scanf("%d", &moisEnter);
+        strcpy(joueur[numeroJoueurs].moisInc, lesMois[moisEnter - 1]);
+            // annee
+        printf("1 - enter l'annee: ");
+        scanf("%d", &joueur[numeroJoueurs].anneeInc);
+        // statue
+        printf("1 - titulaire\n");
+        printf("2 - remplaçant\n");
+        printf("Veuillez indique si le joueur %d est"GREEN" titulaire "RESET"ou" RED" remplacant"RESET": ", numeroJoueurs + 1);
+        scanf("%d", &choix2);
+        switch (choix2) {
+            case 1: strcpy(joueur[numeroJoueurs].status, "titulaire");
+            break;
+            case 2: strcpy(joueur[numeroJoueurs].status, "remplacant");
+            break;
+        }
         id++;
         numeroJoueurs++;
     } while (numeroJoueurs < nombreAjouter + njOriginal);
@@ -134,7 +245,18 @@ void AfficherParOrdreAlphabetique() {
     }
     for (i = 0; i < numeroJoueurs; i++) {
         printf("\n");
-        printf("id: %d | nom: %s | prenom: %s | N maillot: %d | poste: %s | age: %d | N Buts: %d", joueur[i].idUnique, joueur[i].nom, joueur[i].prenom, joueur[i].numeroMaillot, joueur[i].posteJoueur, joueur[i].ageJoueur, joueur[i].numeroButs);
+        printf("id: %d | nom: %s | prenom: %s | N maillot: %d | poste: %s | age: %d | N Buts: %d | Date D'inscription: %d-%s-%d | Statue: %s",
+            joueur[i].idUnique,
+            joueur[i].nom,
+            joueur[i].prenom,
+            joueur[i].numeroMaillot,
+            joueur[i].posteJoueur,
+            joueur[i].ageJoueur,
+            joueur[i].numeroButs,
+            joueur[i].jourInc,
+            joueur[i].moisInc,
+            joueur[i].anneeInc,
+            joueur[i].status);
     }
     printf("\n\n");
 }
@@ -142,7 +264,7 @@ void AfficherParAge() {
     int i=0, j=0;
     for (i = 0; i < numeroJoueurs - 1; i++) {
         for (j = 0; j < numeroJoueurs - i - 1; j++) {
-            if (joueur[j].ageJoueur > joueur[j + 1].ageJoueur) {
+            if (joueur[j].ageJoueur < joueur[j + 1].ageJoueur) {
                 struct Joueurs temp = joueur[j + 1];
                 joueur[j + 1] = joueur[j];
                 joueur[j] = temp;
@@ -151,7 +273,18 @@ void AfficherParAge() {
     }
     for (i = 0; i < numeroJoueurs; i++) {
         printf("\n");
-        printf("id: %d | nom: %s | prenom: %s | N maillot: %d | poste: %s | age: %d | N Buts: %d", joueur[i].idUnique, joueur[i].nom, joueur[i].prenom, joueur[i].numeroMaillot, joueur[i].posteJoueur, joueur[i].ageJoueur, joueur[i].numeroButs);
+        printf("id: %d | nom: %s | prenom: %s | N maillot: %d | poste: %s | age: %d | N Buts: %d | Date D'inscription: %d-%s-%d | Statue: %s",
+            joueur[i].idUnique,
+            joueur[i].nom,
+            joueur[i].prenom,
+            joueur[i].numeroMaillot,
+            joueur[i].posteJoueur,
+            joueur[i].ageJoueur,
+            joueur[i].numeroButs,
+            joueur[i].jourInc,
+            joueur[i].moisInc,
+            joueur[i].anneeInc,
+            joueur[i].status);
     }
     printf("\n\n");
 }
@@ -168,7 +301,18 @@ void AfficherParPoste() {
     }
     for (i = 0; i < numeroJoueurs; i++) {
         printf("\n");
-        printf("id: %d | nom: %s | prenom: %s | N maillot: %d | poste: %s | age: %d | N Buts: %d", joueur[i].idUnique, joueur[i].nom, joueur[i].prenom, joueur[i].numeroMaillot, joueur[i].posteJoueur, joueur[i].ageJoueur, joueur[i].numeroButs);
+        printf("id: %d | nom: %s | prenom: %s | N maillot: %d | poste: %s | age: %d | N Buts: %d | Date D'inscription: %d-%s-%d | Statue: %s",
+            joueur[i].idUnique,
+            joueur[i].nom,
+            joueur[i].prenom,
+            joueur[i].numeroMaillot,
+            joueur[i].posteJoueur,
+            joueur[i].ageJoueur,
+            joueur[i].numeroButs,
+            joueur[i].jourInc,
+            joueur[i].moisInc,
+            joueur[i].anneeInc,
+            joueur[i].status);
     }
     printf("\n\n");
 }
@@ -293,13 +437,17 @@ void Supprimer() {
         printf("\n");
         printf("Veuillez enter le Identifiant de joueur a supprimer: ");
         scanf("%d", &idSupprimer);
-        int i=0;
+        int i=0, j=0;
         for (i = 0; i < numeroJoueurs; i++) {
             if (joueur[i].idUnique == idSupprimer) {
-                joueur[i] = joueur[i + 1];
+                for (j = i; j < numeroJoueurs - 1; j++) {
+                    joueur[j] = joueur[j + 1];
+                }
                 numeroJoueurs--;
+                break;
             }
         }
+        printf("\n");
     }
 }
 
@@ -359,28 +507,64 @@ void Statistiques() {
         printf("Aucun Joueur dans la list");
         printf("\n\n");
     } else {
-        printf("\n-- Statistiques --\n");
-        printf("\n");
-
         //Buts
         int plusMarque;
         printf("Veuillez choisiser le nombre de but (Joueur plus marque): ");
         scanf("%d", &plusMarque);
+        printf("\n-- Statistiques --\n");
+        printf("\n");
         // Total Joueur
-        printf("\nLe nombre total de joueur dans l'equipe: %d\n", numeroJoueurs);
+        printf("\n1 - Le nombre total des joueurs dans l'equipe est: %d\n", numeroJoueurs);
         // Moyenne
-        int i, somme;
+        int i, somme=0;
         for (i = 0; i < numeroJoueurs; i++) {
             somme += joueur[i].ageJoueur;
         }
-        printf("La moyen de l'age des joueurs d'equipe: %d\n", somme / numeroJoueurs);
-        printf("\n");
+        printf("2 - La moyen de l'age des joueurs d'equipe est: %d\n", somme / numeroJoueurs);
         // But plus marque:
-
+        printf("3 - Les joueur qui ont marque plus de %d buts sont:", plusMarque);
+        for (i = 0; i < numeroJoueurs; i++) {
+            if (joueur[i].numeroButs > plusMarque) {
+                printf("\nid: %d | nom: %s | prenom: %s | N maillot: %d | poste: %s | age: %d | N Buts: %d\n", joueur[i].idUnique, joueur[i].nom, joueur[i].prenom, joueur[i].numeroMaillot, joueur[i].posteJoueur, joueur[i].ageJoueur, joueur[i].numeroButs);
+            }
+        }
+        printf("\n");
         // Meilleur buteur:
-
+        int max = joueur[0].numeroButs;
+        for (i = 0; i < numeroJoueurs; i++) {
+            if (joueur[i].numeroButs > max) {
+                max = joueur[i].numeroButs;
+            }
+        }
+        for (i = 0; i < numeroJoueurs; i++) {
+            if (joueur[i].numeroButs == max) {
+                printf("4 - Le meilleur buteur dans l'equipe est %s %s avec %d buts\n", joueur[i].nom, joueur[i].prenom, joueur[i].numeroButs);
+            }
+        }
         // plus jeune plus age
+        int j=0;
+        for (i = 0; i < numeroJoueurs - 1; i++) {
+            for (j = 0; j < numeroJoueurs - i - 1; j++) {
+                if (joueur[j].ageJoueur > joueur[j + 1].ageJoueur) {
+                    struct Joueurs temp = joueur[j + 1];
+                    joueur[j + 1] = joueur[j];
+                    joueur[j] = temp;
+                }
+            }
+        }
+        printf("5 - Le joueur le plus jeune et: %s %s a l'age %d\n", joueur[0].nom, joueur[0].prenom, joueur[0].ageJoueur);
+        printf("6 - Le joueur le plus le plus age et: %s %s a l'age %d\n", joueur[numeroJoueurs - 1].nom, joueur[numeroJoueurs - 1].prenom, joueur[numeroJoueurs - 1].ageJoueur);
     }
+}
+
+int counter = 0, i;
+do {
+
+} while (i < numeroJoueurs);
+
+
+for (i = 0; i < numeroJoueurs; i++) {
+    if (joueur[i].numeroPost )
 }
 
 int main () {
